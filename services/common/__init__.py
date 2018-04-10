d8a2531e3b3ca7239e63dbfdabcfe0b4dce6685c
SERGIO QUEIROZ <sergio.queiroz@buildnprice.io>
2018-04-10 11:22:40 -0300

    Código ajustado para uso da biblioteca http.client

---
+++ services/common/__init__.py
@@ -0,0 +1,8 @@
+import json
+import os
+import logging
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
--- services/products/get.py
+++ services/products/get.py
@@ -1,30 +1,31 @@
 import os
 import json
 import http.client
-# from aws_xray_sdk.core import xray_recorder
-# from aws_xray_sdk.core import patch
+import logging
+from botocore.vendored import requests
 
-# patch(['botocore'])
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
-# @xray_recorder.capture('products/get/{id}')
-def get(event, context):
-  url = os.environ.get('SUPPLIER_API_HOSTNAME')
-  port = os.environ.get('SUPPLIER_API_PORT')
-  app_id = os.environ.get('SUPPLIER_API_APPID')
-  token = os.environ.get('SUPPLIER_API_TOKEN')
-  mode = os.environ.get('SUPPLIER_API_METHOD')
+def handler(event, context):
+  log.debug('Received event {}'.format(json.dumps(event)))
+
+  SUPPLIER_API_HOSTNAME = os.environ.get('SUPPLIER_API_HOSTNAME')
+  SUPPLIER_API_PORT = os.environ.get('SUPPLIER_API_PORT')
+  SUPPLIER_API_APPID = os.environ.get('SUPPLIER_API_APPID')
+  SUPPLIER_API_TOKEN = os.environ.get('SUPPLIER_API_TOKEN')
+  SUPPLIER_API_METHOD = os.environ.get('SUPPLIER_API_METHOD')
   product_id = event['pathParameters']['id']
   
-  conn = http.client.HTTPConnection(url, port)
-  payload = "{\n  \"app_id\": \"" + app_id \
-    +"\",\n  \"token\": \"" + token \
-    +"\",\n  \"mode\": \"" + mode \
+  conn = http.client.HTTPConnection(SUPPLIER_API_HOSTNAME, SUPPLIER_API_PORT)
+  payload = "{\n  \"app_id\": \"" + SUPPLIER_API_APPID \
+      + "\",\n  \"token\": \"" + SUPPLIER_API_TOKEN \
+      + "\",\n  \"mode\": \"" + SUPPLIER_API_METHOD \
     + "\",\n  \"produtos\": [\"" + product_id + "\"]\n}"
   
-  print(payload)
   headers = {
     'content-type': "application/json",
-    'host': "{}:{}".format(url, port),
+    'host': "{}:{}".format(SUPPLIER_API_HOSTNAME, SUPPLIER_API_PORT),
     'x-amz-date': "20170807T191827Z",
     'authorization': "AWS4-HMAC-SHA256 Credential=/20170807/us-east-1/execute-api/aws4_request, SignedHeaders=content-type;host;x-amz-date, Signature=0552d73111e76ef923d9379c98dfdbdca45e5231c1ea9b74843fe916cc3d3ff7",
     'cache-control': "no-cache",
@@ -35,7 +36,18 @@ def get(event, context):
   res = conn.getresponse()
   data = res.read()
   body = data.decode("utf-8")
-  # xray_recorder.current_subsegment().put_annotation('http.client.response', body)
+
+  # data = {'api_option': 'paste',
+  #         'api_user_key': '',
+  #         'api_paste_private': '0',
+  #         'api_paste_name': 'AWS Lambda python modules',
+  #         'api_paste_expire_date': '1D',
+  #         'api_paste_format': 'text',
+  #         'api_dev_key': SUPPLIER_API_TOKEN,
+  #         'api_paste_code': body}
+  # r = requests.post("http://pastebin.com/api/api_post.php", data=data)
+
+  log.debug('Get response: {}'.format(json.dumps(body)))
   response = {
     "statusCode": 200,
     "body": json.dumps(data.decode("utf-8"))
