635d0657d897ccbc34e9e409bb8e31935a96ca07
SERGIO QUEIROZ <sergio.queiroz@buildnprice.io>
2018-04-12 17:52:10 -0300

    Configured canary deploy 10% by 5 min at CodeDeploy

--- services/products/command.sh
+++ services/products/command.sh
@@ -3,6 +3,7 @@ npm install --save serverless-python-requirements
 npm install --save raven
 npm install --save serverless-sentry-lib
 npm install --save-dev serverless-sentry
+npm install --save-dev serverless-plugin-canary-deployments
 pip install raven
 pip freeze > requirements.txt
 sls deploy
